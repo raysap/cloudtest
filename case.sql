d7cc1d51e5bdb750ebc811ae335837bcfdbc83be
unknown <saptarshi.21ray@gmail.com>
2022-01-13 18:35:24 +0530

    loop added

--- case.sql
+++ case.sql
@@ -9,3 +9,26 @@ else 'no result'
 end ;
 dbms_output.put_line ('student'|| ' ' || v_char);
 end;
+
+---------- loop basic nested if else loop---------
+
+DECLARE
+    v_number NUMBER(20) := 0;
+BEGIN
+    LOOP
+        dbms_output.put_line(v_number);
+        v_number := v_number + 1;
+        IF v_number > 10 THEN
+            LOOP
+                dbms_output.put_line('my 2nd loop works' || v_number);
+                v_number := v_number + 2;
+                IF v_number > 20 THEN
+                    EXIT;
+                END IF;
+            END LOOP;
+
+            EXIT;
+        END IF;
+
+    END LOOP;
+END;
