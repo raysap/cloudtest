5917fad13225ac5ce049aa93c06626ef9b1c6145
unknown <saptarshi.21ray@gmail.com>
2022-01-14 14:31:53 +0530

    record complex

--- case.sql
+++ case.sql
@@ -42,3 +42,15 @@ select * into r_jobs from job_copy where job_id = 'AD_VP';
 dbms_output.put_line (r_jobs.min_salary);
 end;
 
+---------complex record------------------------
+
+declare
+--r_jobs job_copy%rowtype;
+type t_emp is record (job_id varchar2(10),job_title job_copy.job_title%type, emp job_history%rowtype);
+r_jobs t_emp;
+begin 
+---dbms_output.put_line (r_jobs.emp.employee_id);
+select job_copy.job_id,job_copy.job_title,job_history.end_date into r_jobs.job_id,r_jobs.job_title,r_jobs.emp.end_date from job_copy,job_history where job_copy.job_id = 'AC_MGR'and job_copy.job_id = job_history.job_id;
+--r_jobs.job_id := 'AD_VP';or
+dbms_output.put_line (r_jobs.emp.end_date);
+end;
